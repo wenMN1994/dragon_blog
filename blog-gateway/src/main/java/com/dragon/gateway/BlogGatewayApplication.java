99285b1ceee613de9552e00a20064ac798fe4828
DragonWen <18475536452@163.com>
2020-10-10 17:26:27 +0800

    创建网关服务

---
+++ blog-gateway/src/main/java/com/dragon/gateway/BlogGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.dragon.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DragonWen
+ */
+@SpringBootApplication
+public class BlogGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogGatewayApplication.class, args);
+    }
+
+}
---
+++ blog-gateway/src/test/java/com/dragon/gateway/BlogGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
