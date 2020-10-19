4b042904693d51b43aade8e882474b3f0672a339
DragonWen <18475536452@163.com>
2020-10-19 22:21:06 +0800

    网关配置

--- blog-gateway/src/main/java/com/dragon/gateway/BlogGatewayApplication.java
+++ blog-gateway/src/main/java/com/dragon/gateway/BlogGatewayApplication.java
@@ -2,11 +2,14 @@ package com.dragon.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author DragonWen
  */
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BlogGatewayApplication {
 
     public static void main(String[] args) {
