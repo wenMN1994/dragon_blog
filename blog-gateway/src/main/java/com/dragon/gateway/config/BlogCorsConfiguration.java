408721fa8f188078c4c8acbeb41311ac61884e87
DragonWen <18475536452@163.com>
2020-10-21 22:25:37 +0800

    网关统一配置跨域

---
+++ blog-gateway/src/main/java/com/dragon/gateway/config/BlogCorsConfiguration.java
@@ -0,0 +1,35 @@
+package com.dragon.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2020/10/21 22:05
+ * @description：处理跨域配置文件
+ * @modified By：
+ * @version: $
+ */
+@Configuration
+public class BlogCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        //1、配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
