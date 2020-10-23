69878b8a0c6f21e09e27aca170a0acbbac4945ba
DragonWen <18475536452@163.com>
2020-10-23 23:06:52 +0800

    单元测试:阿里云对象存储上传

--- blog-third-party/src/test/java/com/dragon/thirdparty/BlogThirdPartyApplicationTests.java
+++ blog-third-party/src/test/java/com/dragon/thirdparty/BlogThirdPartyApplicationTests.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogThirdPartyApplicationTests {
@@ -14,5 +18,22 @@ public class BlogThirdPartyApplicationTests {
     @Test
     public void contextLoads() {
     }
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+
+        // 上传文件流。
+        InputStream inputStream = new FileInputStream("C:\\Users\\DragonWen\\Desktop\\小程序图标\\weichart.png");
+
+        ossClient.putObject("blog-dragon", "weichart.jpg", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+
+        System.out.println("上传完成...");
+    }
+
 
 }
