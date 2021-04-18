bf45f968d0c85f3d6cf2539bc53a2f3609d36111
DragonWen <18475536452@163.com>
2021-04-18 16:06:47 +0800

    整理依赖

--- blog-admin/src/main/java/com/dragon/admin/framework/security/filter/JwtAuthenticationTokenFilter.java
+++ blog-admin/src/main/java/com/dragon/admin/framework/security/filter/JwtAuthenticationTokenFilter.java
@@ -5,6 +5,8 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.dragon.common.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
